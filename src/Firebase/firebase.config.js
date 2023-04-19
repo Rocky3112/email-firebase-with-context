c391f6dd3b1661129f98fe0d77fe0689b32c49f4
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-04-19 21:39:46 +0600

    login & register page with connection

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFQlcxq_d8CuQvScztDjzUwTOY_SARiqw",
+  authDomain: "auth-firebase-context-ta-118de.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-118de",
+  storageBucket: "auth-firebase-context-ta-118de.appspot.com",
+  messagingSenderId: "272899881759",
+  appId: "1:272899881759:web:b819678918eb8bc98f4e2a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../Providers/AuthProvider';
+
+const Home = () => {
+    const user = useContext(AuthContext)
+    return (
+        <div>
+            <h1>this is home {user && <span>{user.displayName}</span>}</h1>
+        </div>
+    );
+};
+
+export default Home;
