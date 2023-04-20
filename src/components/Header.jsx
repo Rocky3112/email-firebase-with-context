30c8678e510c2569cc39691193a16c948a69dd20
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-04-20 10:12:02 +0600

    signout button added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Providers/AuthProvider";
 
 const Header = () => {
+
+  const {user, logOut} =useContext(AuthContext)
+
+  const handleLogOut =()=>{
+    logOut()
+    .then(()=>{})
+    .catch(error => console.log(error))
+  }
   return (
     <div>
       
@@ -10,6 +19,15 @@ const Header = () => {
         <Link className="btn btn-ghost normal-case text-xl" to="/">Home</Link>
         <Link className="btn btn-ghost normal-case text-xl" to="/login">Login</Link>
         <Link className="btn btn-ghost normal-case text-xl" to="/register">Register</Link>
+
+        {
+          user ?<>
+          <span>{user.email}</span>
+          <button onClick={handleLogOut} className="btn btn-xs">SignOut</button>
+          
+          </> 
+          :<Link to ="/login">Login</Link>
+        }
       </div>
     </div>
   );
