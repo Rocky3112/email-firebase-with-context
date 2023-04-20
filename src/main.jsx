619e59f13febc1403aec7dcd7ef0751b8b77ba0b
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-04-20 11:22:35 +0600

    private route setup done

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,9 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import AuthProvider from './Providers/AuthProvider';
+import Orders from './components/Orders';
+import PrivateRoute from './routes/PrivateRoute';
+import Profile from './components/Profile';
 
 const router = createBrowserRouter([
   {
@@ -29,6 +32,14 @@ const router = createBrowserRouter([
       {
         path:'/register',
         element:<Register></Register>
+      },
+      {
+        path:'/orders',
+        element:<PrivateRoute><Orders></Orders></PrivateRoute>
+      },
+      {
+        path:'/profile',
+        element:<PrivateRoute><Profile></Profile></PrivateRoute>
       },
     ]
   },
